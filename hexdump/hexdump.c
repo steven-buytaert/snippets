4f9fcd88240833e94baa7d538da502652daa4a10
Steven Buytaert <Steven@Buytaert.net>
2021-02-02 06:27:02 +0100

    Revamped hexdump. Now starts at 16 byte boundary and return value now like printf.

--- hexdump/hexdump.c
+++ hexdump/hexdump.c
@@ -6,49 +6,164 @@
 #include <string.h>
 #include <stdint.h>
 
-uint32_t hex2buf(char b[], uint32_t s, const void * d2d, uint32_t n, uint32_t off, uint32_t horcap) {
+#include <hexdump.h>
 
-  const uint8_t * data = d2d;
-  uint8_t   hex[64];                                        // The hex dump of the data
-  uint32_t  i;
-  uint32_t  j;
-  
-  union {
-    uint32_t u;                                             // Offset in output buffer
-    int32_t  r;
-  } Off = { 0 };
+typedef struct Buf_t * buf_t;
 
-#define out(b, Off, s, f, a...) { if (Off.u < s) { Off.r += snprintf(b + Off.u, s - Off.u, f, ##a); } }
+typedef struct Buf_t {
+  char * space;                   // Points to the space in the buffer
+  char * cur;                     // Current position in buffer to add characters
+  char * end;                     // Comes just after last character of usable storage; can not be written to
+} Buf_t;
 
-  if (horcap) {
-    out(b, Off, s, "                0   1   2   3   4   5   6   7   8   9   a   b   c   d   e   f\n");
+typedef struct Itoa_t {           // Buffer large enough to build an 8 byte hexadecimal representation
+  char * str;                     // Hexadecimal result string after uitoh call
+  char   space[23];
+  char   end;
+} Itoa_t;
+
+static void addstr(buf_t buf, const char * s, uint32_t n) { // Add the string 's' with the given number of characters 'n' to the buffer
+  memcpy(buf->cur, s, n);
+  buf->cur += n;
+}
+
+static uint32_t remain(buf_t b) {                           // Return remaining buffer space, but ensure we have a spot for the trailing \0
+  return (b->cur < b->end) ? (b->end - 1 - b->cur) : 0;
+}
+
+static uint32_t used(buf_t b) {                             // Return number of characters used in the buffer
+  return (b->cur - b->space);
+}
+
+static void copy2out(buf_t out, buf_t buf) {                // Copy the given buffer to the output buffer, if space permits
+
+  uint32_t u = used(buf);
+  uint32_t r = remain(out);
+
+  if (r) { memcpy(out->cur, buf->space, (r > u) ? u : r); } // Copy to out when there's space remaining
+
+  out->cur += u;                                            // The cursor position is always updated since we need to return # characters that would have been written
+
+}
+
+static void flush2out(buf_t out, buf_t hex, buf_t ascii) {  // Try to copy the hex and ascii representations to the output buffer, if space permits
+
+  addstr(hex, "| ", 2);                                     // Add separator between dump area and ascii area
+
+  copy2out(out, hex);
+  copy2out(out, ascii);
+
+  if (remain(out)) { *out->cur = '\n'; }                    // Add a newline if space permits ...
+  out->cur++;                                               // ... but always account for it
+
+
+}
+
+static void uitoh(Itoa_t * buf, uint32_t w, uint64_t n) {   // Create the hexadecimal representation of a given width 'w' of unsigned number 'n' in the hex buffer
+
+  buf->str = & buf->end;                                    // Build up backwards, so start at the end
+
+  do {                                                      // Put characters from least to most significant ...
+    *--buf->str = "0123456789abcdef"[n & 0xf];              // ... so predecrement and write digits[n % 16]
+    n >>= 4;                                                // Divide by 16
+  } while (n);
+
+  while ((uint32_t) (& buf->end - buf->str) < w) {          // Pad front with zeros to achieve the proper width
+    *--buf->str = '0';
   }
 
-  j = 0;
-  memset(hex, 0, sizeof(hex));
-  out(b, Off, s, " 0x%08x :  ", off);
-  hex[0] = '|';
+}
 
-  for (i = 0; i < n; i++) {
-    out(b, Off, s, "%02x  ", data[i]);
-    hex[j + 1] = isgraph(data[i]) ? data[i] : '.';
-    j += 1;
-    if (j == 16) {
-      j = 0;
-      out(b, Off, s, "  %s|\n 0x%08x :  ", hex, off + i + 1);
-      memset(hex, 0, sizeof(hex));
-      hex[0] = '|';
+static void istart(buf_t hex, buf_t ascii, uint32_t addr) { // Reset ascii and hex buffers and start writing a new line with the given address
+
+  Itoa_t Itoa;
+
+  hex->cur = hex->space;                                    // Reset hex buffer
+  ascii->cur = ascii->space;                                // Reset ascii buffer
+
+  uitoh(& Itoa, 8, addr);                                   // Convert address to hex and output it nicely
+  addstr(hex, "0x", 2);
+  addstr(hex, Itoa.str, 8);
+  addstr(hex, ": ", 2);
+
+}
+
+static void outhex(buf_t buf, uint32_t byte) {              // Output the given byte as a hexadecimal character in the given buffer
+
+  Itoa_t Itoa;
+
+  uitoh(& Itoa, 2, byte);
+  addstr(buf, Itoa.str, 2);
+  addstr(buf, " ", 1);                                      // Add a space after each byte
+
+}
+
+uint32_t hex2buf(char b[], uint32_t s, const void * d, uint32_t n) {
+
+  union {
+    const uint8_t * dump;
+    uint32_t  addr;
+  } Data = { .dump = d };
+
+  uint32_t        iaddr = (Data.addr) & (0 - 16);           // Initial address to dump, aligned on a 16 byte boundary, could be equal to d address argument
+  const uint8_t * end = Data.dump + n;
+  Buf_t           Out;
+
+  struct {
+    Buf_t Buf;
+    char  space[32];
+  } Ascii;                                                  // ASCII area buffer; note that end is not used
+
+  struct {
+    Buf_t Buf;
+    char  space[64];
+  } Hex;                                                    // Address and hex area buffer; note that end is not used
+  
+  Hex.Buf.space = Hex.space;                                // Initialize both area buffers
+  Ascii.Buf.space = Ascii.space;
+
+  Out.cur = b;                                              // Initialize the output buffer
+  Out.space = b;
+  s = b ? s : 0;                                            // Set size to 0 when no buffer was given
+  Out.end = b + s;
+
+#define ascii(c)  { *Ascii.Buf.cur++ = (c);                   }
+#define out(s, n) { addstr(& Hex.Buf, s, n);                  }
+#define hex(x)    { outhex(& Hex.Buf, x);                     }
+#define flush     { flush2out(& Out, & Hex.Buf, & Ascii.Buf); }
+#define start(a)  { istart(& Hex.Buf, & Ascii.Buf, a);        }
+
+  start(iaddr);
+
+  while (iaddr++ < Data.addr) {                             // We start dumping a 16 byte aligned addresses, so output some padding if required
+    out("   ", 3); ascii(' ');
+  }
+
+  while (Data.dump < end) {
+    hex(Data.dump[0]);                                      // Output hexadecimal ...
+    ascii(isprint(Data.dump[0]) ? Data.dump[0] : '.');      // ... and ascii representation, if any, of the current byte
+    Data.dump++;                                            // Go to next byte, if any
+    if (0 == (0xf & Data.addr)) {                           // Next address is a 16 byte aligned address ...
+      flush;                                                // ... flush this line ...
+      if (Data.dump < end) {                                // ... and start the next one if there's data to dump left
+        start(Data.addr);
+      }
     }
   }
 
-  if (j != 0) {
-    for (i = j; i < 16; i++) { out(b, Off, s, "--  "); }
-    out(b, Off, s, "  %s", hex);
-    for (i = j; i < 16; i++) { out(b, Off, s, "-");    }
-    out(b, Off, s, "|");
+  if (0xf & Data.addr) {                                    // Address not 16 byte aligned, we have some remainder ascii representation
+    for ( ; 0xf & Data.addr; ++Data.addr) {
+      out("   ", 3);                                        // Output some padding to align the ascii dump area ...
+    }
+    flush;                                                  // Flush the final line with the ascii remainder
   }
 
-  return (int) (s - Off.u) > 0 ? Off.u : s;
+  if (s) {                                                  // There's always a spot for a trailing \0, if there was a buffer.
+    if (remain(& Out)) { Out.cur[0] = 0; }
+    else { Out.end[-1] = 0; }                               // Buffer wasn't large enough, cur progressed beyond end.
+  }
+
+  return (uint32_t) (Out.cur - Out.space);
 
 }
 
@@ -58,7 +173,7 @@ void hexdump8k(const void * d2d, uint32_t n, const char * msg) {
 
   n = (n > 512) ? 512 : n;
 
-  hex2buf(buf, sizeof(buf) - 1, d2d, n, 0, 1);
+  hex2buf(buf, sizeof(buf) - 1, d2d, n);
 
   printf("%s\n%s\n", msg, buf);
 
--- hexdump/hexdump.h
+++ hexdump/hexdump.h
@@ -7,12 +7,12 @@
 
 // Pass a buffer and size to format into a hexdump of data pointed at by 'd2d' and n
 // bytes long; the address shown, starts at 'off'. Return the number of bytes that
-// are effectively written, including the \0. When horcap is not zero, a horizontal
-// line with address offsets is printed.
+// would have been written, if the buffer was large enough. Buffer can be NULL. The
+// output will be aligned on a 16 byte boundary.
 
-uint32_t hex2buf(char b[], uint32_t s, const void * d2d, uint32_t n, uint32_t off, uint32_t horcap);
+uint32_t hex2buf(char b[], uint32_t s, const void * d2d, uint32_t n);
 
-// Convenience function and example; given buffer is 8k and offset passed as '0'.
+// Convenience function and example; given buffer is 8k.
 // The passed message is printed as title.
 
 void hexdump8k(const void * d2d, uint32_t n, const char * msg);
