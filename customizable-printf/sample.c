ea412558a8e3252cd86ae19824915e8fafa0bb8e
Steven Buytaert <Steven@Buytaert.net>
2023-08-30 16:26:53 +0200

    Added some missing header dependencies.

--- customizable-printf/sample.c
+++ customizable-printf/sample.c
@@ -1,4 +1,4 @@
-// Copyright 2020 Steven Buytaert.
+// Copyright 2020, 2023 Steven Buytaert.
 
 #include <stdint.h>
 
@@ -8,11 +8,11 @@ int main(int argc, char * argv[]) {
 
   uint32_t keyvalue = (100 << 3) | 3;                       // Table 3 entry 100.
 
-  myprintf("Hello %s. Key %k\n", "world", keyvalue);
+  myprintf("Hello %s. Key %k\n", "world", keyvalue);        // Demonstrates the custom %k formatter.
 
-  myprintf("Binary %032b\n", keyvalue);
+  myprintf("Binary %032b\n", keyvalue);                     // Demonstrates the binary %b formatter.
   
-  myprintf("Planck length %17e\n", 1.61622837e-35);
+  myprintf("Planck length %17e\n", 1.61622837e-35);         // Proper IEEE754 formatting.
 
   return 0;
 
